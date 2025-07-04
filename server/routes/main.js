ad59d54e9befae7725d9a3cd9eb36bf0b13de164
Vuk Mudrinic <mudrinicvuk@gmail.com>
2025-07-04 12:04:23 +0200

    Update main.js

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const narudzbine = require('../models/narudzbine');
 
+const add_payment_info_layout = '../views/layouts/add_payment_info';
+const checkout_initiated_layout = '../views/layouts/checkout_initiated';
+const purchase_layout = '../views/layouts/purchase';
+const view_content_layout = '../views/layouts/view_content';
+
 // Routes
 router.get('', (req, res) => {
     res.render('index'); 
@@ -12,65 +17,65 @@ router.get('/about', (req, res) => {
 });
 
 router.get('/katalog', (req, res) => {
-    res.render('katalog'); 
+    res.render('katalog', { layout: view_content_layout }); 
 });
 
 //---------------------Klime------------------------
 router.get('/gree-pular-12k', (req, res) => {
-    res.render('klime/gree-pular-12k'); 
+    res.render('klime/gree-pular-12k', { layout: view_content_layout }); 
 });
 
 router.get('/gree-clivia-ai-12k', (req, res) => {
-    res.render('klime/gree-clivia-ai-12k'); 
+    res.render('klime/gree-clivia-ai-12k', { layout: view_content_layout }); 
 });
 
 router.get('/gree-clivia-Ai-12k-black', (req, res) => {
-    res.render('klime/gree-clivia-ai-12k-black'); 
+    res.render('klime/gree-clivia-ai-12k-black', { layout: view_content_layout }); 
 });
 
 router.get('/gree-amber-pro-12k', (req, res) => {
-    res.render('klime/gree-amber-pro-12k'); 
+    res.render('klime/gree-amber-pro-12k', { layout: view_content_layout }); 
 });
 
 router.get('/bergen-kron-smart-12k', (req, res) => {
-    res.render('klime/bergen-kron-smart-12k'); 
+    res.render('klime/bergen-kron-smart-12k', { layout: view_content_layout }); 
 });
 
 router.get('/bergen-kron-smart-12k-black', (req, res) => {
-    res.render('klime/bergen-kron-smart-12k-black'); 
+    res.render('klime/bergen-kron-smart-12k-black', { layout: view_content_layout }); 
 });
 
 router.get('/bergen-apollo-t-12k', (req, res) => {
-    res.render('klime/bergen-apollo-t-12k'); 
+    res.render('klime/bergen-apollo-t-12k', { layout: view_content_layout }); 
 });
 
 //----------------Poruci Klime-----------------------
 router.get('/poruci-gree-pular-12k', (req, res) => {
-    res.render('poruci-klimu/poruci-gree-pular-12k'); 
+    res.render('poruci-klimu/poruci-gree-pular-12k', { layout: checkout_initiated_layout }); 
 });
 
 router.get('/poruci-gree-clivia-ai-12k', (req, res) => {
-    res.render('poruci-klimu/poruci-gree-clivia-ai-12k'); 
+    res.render('poruci-klimu/poruci-gree-clivia-ai-12k', { layout: checkout_initiated_layout }); 
 });
 
 router.get('/poruci-clivia-Ai-12k-black', (req, res) => {
-    res.render('poruci-klimu/poruci-clivia-Ai-12k-black'); 
+    res.render('poruci-klimu/poruci-clivia-Ai-12k-black', { layout: checkout_initiated_layout }); 
 });
 
 router.get('/poruci-amber-pro-12k', (req, res) => {
-    res.render('poruci-klimu/poruci-amber-pro-12k'); 
+    res.render('poruci-klimu/poruci-amber-pro-12k', { layout: checkout_initiated_layout }); 
 });
 
 router.get('/poruci-kron-smart-12k', (req, res) => {
-    res.render('poruci-klimu/poruci-kron-smart-12k'); 
+    res.render('poruci-klimu/poruci-kron-smart-12k', { layout: checkout_initiated_layout }); 
 });
 
 router.get('/poruci-kron-smart-12k-black', (req, res) => {
-    res.render('poruci-klimu/poruci-kron-smart-12k-black'); 
+    res.render('poruci-klimu/poruci-kron-smart-12k-black', { layout: checkout_initiated_layout }); 
 });
 
 router.get('/poruci-apollo-t-12k', (req, res) => {
-    res.render('poruci-klimu/poruci-apollo-t-12k'); 
+    res.render('poruci-klimu/poruci-apollo-t-12k', { layout: checkout_initiated_layout });
 });
 
 //------------Updejtuj porudzbinu u bazu podataka--------
@@ -208,30 +213,3 @@ router.post('/post-poruci-bergen-kron-smart-12k-black', async(req, res) => {
 });
 
 module.exports = router;
-
-//---------bezkorisni-primeri-----------------------
-
-//---------------ubacuje-narudzbine-u-db------------
-    // function ubaciNarudzbinuUDB() {
-    //     narudzbine.insertMany([
-    //         {
-    //             ime: "Vuk",
-    //             prezime: "Mudrinic",
-    //             telefon: "0615566989",
-    //             grad: "Novi Sad",
-    //             mesto: "Liman 2",
-    //             ulica: "Resavska 4",
-    //             postanski: "21000",
-    //         },
-    //         {
-    //             ime: "Nebojsa",
-    //             prezime: "Jovanovic",
-    //             telefon: "0615566989",
-    //             grad: "Novi Sad",
-    //             mesto: "Podbara",
-    //             ulica: "Patrijarha Carnojevica 5",
-    //             postanski: "21000",
-    //         },
-    //     ])
-    // }
-    // ubaciNarudzbinuUDB();
